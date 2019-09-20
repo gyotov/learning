7b6193eaa4981eef353b7e71ef6aafc69e791c72
shopifydev-gy <shopifydev@gmail.com>
2019-09-20 17:29:32 +0300

    add latest files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,19 @@
-const chalk = require('chalk');
+const yargs = require('yargs');
+// const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
-console.log(chalk.red('Success'));
+// Customize yargs version
+yargs.version('1.1.0');
+
+// Add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function() {
+    console.log('Adding new note');
+  }
+});
+
+// add, remove, read, list
+
+console.log(yargs.argv);
