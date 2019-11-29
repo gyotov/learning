99ec4176ac6ef6e8511f4748383514847d713872
shopifydev-gy <shopifydev@gmail.com>
2019-11-29 13:56:03 +0200

    Add latest files

--- node.js/web-server/src/app.js
+++ node.js/web-server/src/app.js
@@ -4,7 +4,30 @@ const express = require('express');
 const app = express();
 const publicDirPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Joro'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Joro'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help page',
+    message:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt repudiandae sunt aspernatur veritatis amet, consectetur tenetur recusandae nobis illo dolorum ut nemo nostrum sequi soluta eius, similique autem harum eos.'
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
