92195434b98d97ae748a89a42ea3c11d7d89236b
shopifydev-gy <shopifydev@gmail.com>
2019-12-04 12:25:31 +0200

    Section 8 node.js completed.

--- node.js/web-server/public/js/app.js
+++ node.js/web-server/public/js/app.js
@@ -1,10 +1,24 @@
-fetch('http://localhost:3000/weather?address=Sofia%20Bulgaria').then(res => {
-  res.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
+const weatherForm = document.querySelector('form');
+const searchField = document.querySelector('input');
+const msgOne = document.querySelector('#msg-1');
+const msgTwo = document.querySelector('#msg-2');
+
+weatherForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const location = searchField.value;
+
+  msgOne.textContent = 'Loading...';
+  msgTwo.textContent = '';
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(res => {
+    res.json().then(data => {
+      if (data.error) {
+        msgOne.textContent = data.error;
+      } else {
+        msgOne.textContent = data.location;
+        msgTwo.textContent = data.forecast;
+      }
+    });
   });
 });
